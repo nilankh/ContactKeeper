422d631a0b3b3195e97c8d2d69f58d582147b519
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-09 09:55:05 +0530

    git form component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
 
 const Home = () => {
   return (
     <div className="grid-2">
       <div>
-        {/* contactform */}
+      <ContactForm />
       </div>
       <div>
         <Contacts />
+        
       </div>
     </div>
   );
