92b35b23bd937dc953e1e0fcac3018a1996cf97b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-11 08:49:13 +0530

    Register forms done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
-
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+
 import "./App.css";
 
 const App = () => {
@@ -17,8 +18,10 @@ const App = () => {
             <Navbar />
             <div className="container">
               <Switch>
+
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
               </Switch>
             </div>
           </Fragment>
